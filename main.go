1171d8e997726c2fd6ef9d58bef426b102aa374c
liuxiaoguang <vincexgliu@gamil.com>
2017-11-20 10:33:22 +0800

    fix bug

--- main.go
+++ main.go
@@ -1,29 +1,29 @@
 package main
 
 import (
-    "fmt"
+	"fmt"
+	"log"
 	"net/http"
-    "strings"
-    "log"
+	"strings"
 )
 
 func sayHelloName(w http.ResponseWriter, r *http.Request) {
-    r.ParseForm()
-    fmt.Println(r.Form)
-    fmt.Println("path", r.URL.Path)
-    fmt.Println("scheme", r.)
-    fmt.Println(r.Form["url_long"])
-    for k, v := range r.Form {
-        fmt.Println("key:", k)
-        fmt.Println("val:", strings.join(v, ""))
-    }
-    fmt.Fprintf(w, "Hello astaxie!")
+	r.ParseForm()
+	fmt.Println(r.Form)
+	fmt.Println("path", r.URL.Path)
+	fmt.Println("scheme", r.URL.Scheme)
+	fmt.Println(r.Form["url_long"])
+	for k, v := range r.Form {
+		fmt.Println("key:", k)
+		fmt.Println("val:", strings.Join(v, ""))
+	}
+	fmt.Fprintf(w, "Hello astaxie!")
 }
 
 func main() {
 	http.HandleFunc("/", sayHelloName)
-    err := http.ListenAndServe(":8000", nil)
-    if err != nil {
-        log.Fatal("ListenAndServe: ", err)
-    }
+	err := http.ListenAndServe(":8000", nil)
+	if err != nil {
+		log.Fatal("ListenAndServe: ", err)
+	}
 }
